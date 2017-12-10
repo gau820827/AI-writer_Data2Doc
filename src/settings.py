[gau820827] Fix loss calculation and add <EOS>.

src/settings.py
@@ -2,4 +2,4 @@
 import torch
 file_loc = '../boxscore-data/rotowire/'
 use_cuda = torch.cuda.is_available()
-MAX_LENGTH = 546
+MAX_LENGTH = 630

src/train.py
@@ -130,7 +130,7 @@ def sentenceloss(rt, re, rm, summary, encoder, decoder,
     decoder_input = Variable(torch.LongTensor(batch_length).zero_())
     decoder_input = decoder_input.cuda() if use_cuda else decoder_input
 
-    teacher_forcing_ratio = 0.5
+    teacher_forcing_ratio = 1.0 
     use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
 
     if use_teacher_forcing:
@@ -157,7 +157,7 @@ def sentenceloss(rt, re, rm, summary, encoder, decoder,
 
             loss += criterion(decoder_output, summary[:, di])
             # if ni == '<EOS>':
-            #     break
+            #    break
 
     loss.backward()
 
@@ -183,8 +183,11 @@ def addpaddings(summary):
 
 
 def train(train_set, langs, embedding_size=600, learning_rate=0.01,
-          iter_time=10, batch_size=32, get_loss=1000, save_model=5000):
+          iter_time=10, batch_size=32, show_loss=1, save_model=500):
     """The training procedure."""
+    # Set the record file
+    f = open('baseline.result', 'wt')
+
     # Set the timer
     start = time.time()
 
@@ -236,18 +239,20 @@ def train(train_set, langs, embedding_size=600, learning_rate=0.01,
         loss = sentenceloss(rt, re, rm, summary, encoder, decoder,
                             encoder_optimizer, decoder_optimizer, criterion,
                             embedding_size)
-
         total_loss += loss
 
         # Print the information and save model
-        if iteration % get_loss == 0:
+        if iteration % show_loss == 0:
             print("Time {}, iter {}, avg loss = {:.4f}".format(
-                gettime(start), iteration, total_loss / get_loss))
+                gettime(start), iteration, total_loss / show_loss),
+                file=f)
+            total_loss = 0
         if iteration % save_model == 0:
             torch.save(encoder.state_dict(), "encoder_{}".format(iteration))
             torch.save(decoder.state_dict(), "decoder_{}".format(iteration))
-            print("Save the model at iter {}".format(iteration))
-
+            print("Save the model at iter {}".format(iteration), file=f)
+    
+    f.close()
     return encoder, decoder
 
 
@@ -342,8 +347,8 @@ def main():
     # Default parameter
     embedding_size = 600
     learning_rate = 0.01
-    train_iter_time = 1
-    batch_size = 2
+    train_iter_time = 100000
+    batch_size = 8
 
     # For Training
     train_data, train_lang = loaddata(file_loc, 'train')