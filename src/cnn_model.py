dbe2113fa7484840e674318193f72ef1f7055a36
Huri Ma <huri.ma@nyu.edu>
2017-12-13 06:05:44 -0500

    should debug

--- src/cnn_model.py
+++ src/cnn_model.py
@@ -45,16 +45,15 @@ class Conv_relation_extractor(nn.Module):
         self.linear1 = nn.Linear(len(self.kernel_sizes) * filter_num, free_layer_size)
         self.linear2 = nn.Linear(free_layer_size, output_size)
 
-    def forward(self, inp):
-        word_embed = self.embed_word(inp.word)
-        ent_pos_embed = self.embed_entpos(inp.pos1)
-        num_pos_embed = self.embed_numpos(inp.pos2)
+    def forward(self, sent, ent_pos, num_pos):
+        word_embed = self.embed_word(sent)
+        ent_pos_embed = self.embed_entpos(ent_pos)
+        num_pos_embed = self.embed_numpos(num_pos)
         x = torch.cat( (word_embed, ent_pos_embed, num_pos_embed), 1)
         conv_results = [
             F.max_pool1d(F.relu(self.convs[i]), self.max_len - self.kernel_sizes[i] + 1)
                 .view(-1, self.kernel_sizes[i])
             for i in range(len(self.kernel_sizes))]
-
         x = torch.cat(conv_results, 1)
         x = self.dropout(x)
         x = self.linear1(x)
--- src/data_prepare.py
+++ src/data_prepare.py
@@ -16,20 +16,26 @@ class Prepare_data:
     Read the h5 file and get batches
     """
     def __init__(self, filepath, datatype='tr'):
+        self.datatype = datatype
         self.h5fi = h5py.File(filepath, 'r')
         self.sen = self.h5fi[datatype + 'sents'].value
         self.len = self.h5fi[datatype + 'lens'].value
-        self.numdists= self.h5fi[datatype + 'numdists'].value
         self.entdists = self._shift_nagative(self.h5fi[datatype + 'entdists'].value)
         self.numdists = self._shift_nagative(self.h5fi[datatype + 'numdists'].value)
 
         self.labels = self.h5fi[datatype + 'labels'].value
-        self.labelnum = np.asarray(list(map(lambda x : x[-1], self.labels)))
+        self.labelnum = list(map(lambda x : x[-1], self.labels))
 
         self.label_pad = np.amax(self.labels)
         self.word_pad = np.amax(self.sen) + 1
         self.ent_dist_pad = np.amax(self.entdists) + 1
         self.num_dist_pad = np.amax(self.numdists) + 1
+        import pdb; pdb.set_trace()
+
+        self.sen = self._to_list_and_padding(self.sen, self.word_pad)
+        self.numdists = self._to_list_and_padding(self.numdists, self.num_dist_pad)
+        self.entdists = self._to_list_and_padding(self.entdists, self.ent_dist_pad)
+        self.labels = self._to_list_and_padding(self.labels, self.label_pad)
 
     def __def__ (self):
         self.h5fi.close()
@@ -39,6 +45,15 @@ class Prepare_data:
                   entpos_size=self.ent_dist_pad, numpos_size=self.num_dist_pad, \
                   max_len=np.amax(self.len))
         return size_info
+
+    def _to_list_and_padding(self, x, padding):
+        x = x.tolist()
+        # x = list(map(lambda x : x if != -1 else padding, x))
+        for i in range(len(x)):
+            for j in range(len(x[i])):
+                if x[i][j] == -1:
+                    x[i][j] = padding
+        return x
 
 
     def get_batch(self, batch_size=32):
@@ -51,12 +66,15 @@ class Prepare_data:
                 start = 0
                 random.shuffle(order)
             batch_indices = order[start : start + batch_size]
-            sen_batch = [self.sen[idx] for idx in batch_indices]
+            sen_batch = [self.sen[idx]for idx in batch_indices]
             ent_dist_batch = [self.entdists[idx] for idx in batch_indices]
             num_dist_batch = [self.numdists[idx] for idx in batch_indices]
-            label_batch = [self.labels[idx] for idx in batch_indices]
+            # do somthing else if the datatype is test
             label_num_batch = [self.labelnum[idx] for idx in batch_indices]
+            # label_batch = [self.labels[idx][:self.labelnum[idx]] for idx in batch_indices]
+            label_batch = [self.labels[idx][0] for idx in batch_indices]
             start += batch_size
+            import pdb; pdb.set_trace()
             yield sen_batch, ent_dist_batch, num_dist_batch, label_batch, label_num_batch
         # import pdb; pdb.set_trace()
         # print('hello')
@@ -81,6 +99,6 @@ if __name__ == '__main__':
     parser.add_argument('-save', action='store_true', help='save the model or not')
 
     args = parser.parse_args()
-    data = Prepare_data(args)
+    data = Prepare_data(args.input_path)
     aaa, bbb, ccc, ddd, eee = next(data.get_batch())
     print('hello')
